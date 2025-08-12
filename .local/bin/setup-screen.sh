6a87ecafd25667669d64ec204291bd26b3ee9440
Fredrik Sandhei <fredrik.sandhei@gmail.com>
2025-08-12 15:56:24 +0200

    fix issue with setting up screens too early

--- .local/bin/setup-screen.sh
+++ .local/bin/setup-screen.sh
@@ -2,7 +2,27 @@
 
 main_screen="eDP-1"
 offscreen="DP-1"
-xrandr --output "$main_screen" --auto --output "$offscreen" --auto --right-of "$main_screen"
+offscreen_device="card1-$offscreen"
+
+current_state="$(cat /sys/class/drm/$offscreen_device/status)"
+
+user_id="$(id -u)"
+
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$user_id/bus"
+
+if [ "$current_state" = "connected" ]; then
+    echo "setting up dual-screen"
+    xrandr --output "$main_screen" --auto --output "$offscreen" --auto --right-of "$main_screen"
+    sleep 0.5
+else
+    echo "setting up single screen"
+    xrandr --output "$main_screen" --auto
+    xrandr --output "$offscreen" --off
+fi
 
 # Draw background
 nitrogen --restore
+
+# Restart xmonad, or else we may miss the status bar and
+# other commands being run for the second screen.
+xmonad --restart
