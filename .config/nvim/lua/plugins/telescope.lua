cea9042855a9687fccf6b121651fd048d474d13f
Fredrik Sandhei <fredrik.sandhei@gmail.com>
2026-06-27 17:52:41 +0200

    nvim: Update telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,9 @@
 return {
    'nvim-telescope/telescope.nvim',
-   tag = '0.1.8',
+   tag = 'v0.2.1',
    -- or                              , branch = '0.1.x',
-   dependencies = { 'nvim-lua/plenary.nvim' }
+   dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+   }
 }
